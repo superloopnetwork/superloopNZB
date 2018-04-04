3495a1f23ab0f38292d3a1fc7ea6fd04fb6a0e8c
Wai Lit Loi <wailit@gmail.com>
2018-04-04 16:35:57 +0000

    Building destination modules

---
+++ lib/sjc.py
@@ -0,0 +1,6 @@
+######################## FUNCTIONS ##############################
+
+def sjc(directory):
+	print 'mv ~/movies/%s /mnt/movies/' % directory
+	mv = subprocess.call('mv ~/movies/%s /mnt/movies/' % directory, shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+	print 'mv complete'
