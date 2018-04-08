80ea28cee86099efbb94cc41a8469abd5d740215
Wai Lit Loi <wailit@gmail.com>
2018-04-08 23:30:22 +0000

    Updated command with parallel

--- lib/sjc_sea.py
+++ lib/sjc_sea.py
@@ -3,6 +3,6 @@ import subprocess
 
 def sjc_sea(directory):
 	print 'mv ~/movies/%s /mnt/sanjose/' % directory
-	mv = subprocess.call('tee /mnt/sanjose/%s /mnt/seattle/%s < ~/movies/%s >/dev/null' % (directory,directory,directory), shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-	mv = subprocess.call('rm -rf ~/movies/%s' % directory, shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+	parallel = subprocess.call('parallel cp -rf ~/movies/%s ::: /mnt/sanjose /mnt/seattle/' % (directory), shell=True)
+	rm = subprocess.call('rm -rf ~/movies/%s' % (directory), shell=True) 
 	print 'mv complete'
